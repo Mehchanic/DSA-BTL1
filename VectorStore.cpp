18c403cce7fbbd86c5ed0ef639cf1c478a7b2147
= <=>
2025-09-15 22:50:06 +0700

    define copy Operator

--- VectorStore.cpp
+++ VectorStore.cpp
@@ -53,6 +53,15 @@ void ArrayList<T>::ensureCapacity(int cap) {
     temp = nullptr;
 }
 
+
+template <class T>
+ArrayList<T>& ArrayList<T>::operator=(const ArrayList<T>& other) {
+    //Kiểm tra capacity của other và gốc
+    if (other.capacity > this->capacity) ensureCapacity(other.capacity);
+
+    //Copy qua
+    for (int i = 0; i < other.count; i++) this->data[i] = other.data[i];
+}
 // ----------------- Iterator of ArrayList Implementation -----------------
 template <class T>
 ArrayList<T>::Iterator::Iterator(ArrayList<T>* pList, int index) {
